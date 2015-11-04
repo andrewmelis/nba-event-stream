b37f22139be2b4827db229c0fe5e4706a4f89ab2
andrew melis <andrewmelis@gmail.com>
2015-11-03 21:18:11 -0600

    fix the producer given new hash structure of pbp events

--- nba-harvester/src/nba_harvester/producer.clj
+++ nba-harvester/src/nba_harvester/producer.clj
@@ -2,5 +2,6 @@
   (:require [clj-kafka.new.producer :refer :all]))
 
 (defn publish-nba-pbp-event [team-abbreviation message-body]
-  (with-open [pr (producer {"bootstrap.servers" "localhost:9092"} (byte-array-serializer) (byte-array-serializer))]
-    (send pr (record team-abbreviation (.getBytes message-body)))))
+  (let [message (str (:event-num message-body) " | " (:content message-body))]
+    (with-open [pr (producer {"bootstrap.servers" "localhost:9092"} (byte-array-serializer) (byte-array-serializer))]
+      (send pr (record team-abbreviation (.getBytes message))))))
